41be6f87fc221b0dff0b2a1995ca5aabe08aba68
sxunt <if43vip@gmail.com>
2020-11-06 16:16:27 +0800

    first commit

---
+++ frontend/views/site/contact.php
@@ -0,0 +1,53 @@
+<?php
+
+/* @var $this yii\web\View */
+/* @var $form yii\bootstrap\ActiveForm */
+/* @var $model \frontend\models\ContactForm */
+
+use yii\helpers\Html;
+use yii\bootstrap\ActiveForm;
+use yii\captcha\Captcha;
+
+$this->title = 'Contact';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-contact">
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        If you have business inquiries or other questions, please fill out the following form to contact us. Thank you.
+    </p>
+
+    <div class="row">
+        <div class="col-lg-5">
+            <?php $form = ActiveForm::begin(['id' => 'contact-form']); ?>
+
+                <?= $form->field($model, 'name') ?>
+
+                <?= $form->field($model, 'email') ?>
+
+                <?= $form->field($model, 'subject') ?>
+
+                <?= $form->field($model, 'body')->textArea(['rows' => 6]) ?>
+
+                <div class="avatar-view" style=" margin-bottom:10px;">
+                      <img src="" alt="上传封面图">
+                </div>
+                <input type="hidden" value="/uploads/avatar/20201105/5fa3a6427abe6_big.jpg" id="xxx-upimg" name="ContactForm[subject2]">
+
+                <?= $form->field($model, 'verifyCode')->widget(Captcha::className(), [
+                    'template' => '<div class="row"><div class="col-lg-3">{image}</div><div class="col-lg-6">{input}</div></div>',
+                ]) ?>
+
+                
+                <div class="form-group">
+                    <?= Html::submitButton('Submit', ['class' => 'btn btn-primary', 'name' => 'contact-button']) ?>
+                </div>
+
+            <?php ActiveForm::end(); ?>
+
+            <?= common\widgets\avatar\AvatarWidget::widget(['imageUrl'=>'']); ?>
+        </div>
+    </div>
+
+</div>
